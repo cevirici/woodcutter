cc330684ad19de7e9eb6c38585d860ba5e9a5d17
ceviri <bleh3@hotmail.com>
2018-02-24 13:56:11 +0800

    Prettier js

--- static/woodcutter/main.js
+++ static/woodcutter/main.js
@@ -1,8 +1,5 @@
-$('.controlbox').not(':eq(0)').addClass('faded');
-$('.valuebox').not(':eq(0)').addClass('faded');
-$('.graph-container').not(':eq(0)').css('display','none');
-$('.modalcontrolbox').addClass('faded');
-$('.modal').toggle();
+
+/*********** Card Layer Highlighting ***********/
 
 var highlightTimer;
 
@@ -10,118 +7,120 @@ function rehighlight(){
 	$('.graph-layer').css('opacity',1);
 }
 
+function highlight_layer(targetCard){
+	$('.legendbox[card='+targetCard+']').addClass('highlight');
+	$('.graph-layer').css('opacity',0.4);
+	$('.graph-layer.card'+targetCard).css('opacity',1);
+	clearTimeout(highlightTimer);
+}
+
+function dehighlight_layer(targetCard){
+	$('.legendbox[card='+targetCard+']').removeClass('highlight');
+	highlightTimer = setTimeout(rehighlight,500);
+}
+
 $('.box').hover(
-	function(){	targetCard = $(this).attr('card');
-				$(this).addClass('highlight');
-				$('.legendbox[card='+targetCard+']').addClass('highlight');
-				$('.graph-layer').css('opacity',0.4);
-			    $('.graph-layer.card'+targetCard).css('opacity',1);
-			    clearTimeout(highlightTimer)},
-	function(){	highlightTimer = setTimeout(rehighlight,500);
-				$(this).removeClass('highlight');
-				$('.legendbox[card='+targetCard+']').removeClass('highlight');}
+	function() {highlight_layer($(this).attr('card'))},
+	function() {dehighlight_layer($(this).attr('card'))}
 );
 
 $('.legendbox').hover(
-	function(){	targetCard = $(this).attr('card');
-				$(this).addClass('highlight');
-				$('.graph-layer').css('opacity',0.4);
-			    $('.graph-layer.card'+targetCard).css('opacity',1);
-				clearTimeout(highlightTimer)},
-	function(){	highlightTimer = setTimeout(rehighlight,500);
-				$(this).removeClass('highlight');}
+	function() {highlight_layer($(this).attr('card'))},
+	function() {dehighlight_layer($(this).attr('card'))}
 );
+
+
+/*********** Card Hiding ***********/
 
 $('.legendbox').click(
 	function(){
 		$(this).toggleClass('faded');
-		var dir = ($(this).hasClass('faded') ? -1 : 1)
-		var targetCard = $(this).attr('card');
-		length = $('.axislabel').length;
-		$('.graph-container').each(function(index,graph){
-			for (side=0;side<2;side++){
-				row = $(graph).children('.row:eq('+side+')');
-				killedCount = [];
-				for(var i=0; i<length; ++i) killedCount.push([]);
 
-				$(row).find('.graph-layer.card'+targetCard+' .box').each(function(index,x){
-					killedCount[$(x).attr('xcoord')].push(parseInt($(x).attr('ycoord')));
-					$(x).toggle();
+		let dir = ($(this).hasClass('faded') ? -1 : 1)
+		let targetCard = $(this).attr('card');
+		let length = $('.axislabel').length;
+
+		$('.graph-container').each((i,graph) => {
+			for (let side=0; side<2; ++side){
+				let row = $(graph).children('.row:eq('+side+')');
+				let dirString = ['bottom','top'][side];
+
+				let killedCount = [];
+				for(let i=0; i<length; ++i) killedCount.push([]);
+
+				row.find('.graph-layer.card'+targetCard+' .box').each((i,x) => {
+						killedCount[$(x).attr('xcoord')].push(parseInt($(x).attr('ycoord')));
 				});
-				
-				dirString = ['bottom','top'][side];
 
-				row.find('.box').each(function(index,x){
-					t = parseInt($(x).attr('currenty'));
-					actualY = parseInt($(x).attr('ycoord'));
-					thisKillCount = killedCount[$(x).attr('xcoord')];
-					for(i=0;i<thisKillCount.length;i++){
-						if (thisKillCount[i]<actualY){
-							t += dir
-						}
-					};
-					$(x).attr('currenty',t.toString());
-					actualHeight = (0.5*Math.floor(t/5)+t*2).toString();
+				row.find('.graph-layer.card'+targetCard).toggle();
+
+				row.find('.box').each((i,x) => {
+					let currentY = parseInt($(x).attr('currenty'));
+					let rootY = parseInt($(x).attr('ycoord'));
+					let	columnKills = killedCount[$(x).attr('xcoord')];
+					currentY += dir * columnKills.filter(x => x <rootY).length;
+
+					$(x).attr('currenty', currentY.toString());
+
+					actualHeight = (0.5*Math.floor(currentY/5)+currentY*2).toString();
 					$(x).css(dirString,actualHeight+'%');
 				});
-
 			}
 		});
 	}
 );
 
-$('.axislabel').click(
-	function(){
-		console.log('pop');
-		$('.story-container').scrollTop($('.story-line[turn=\''+$(this).index()+'\']').get(0).offsetTop-10);
-	}
-);
 
-$('.story-sidebar-block').click(
-	function(){
-		console.log('pop');
-		$('.story-container').scrollTop($('.story-line[turn=\''+$(this).index()+'\']').get(0).offsetTop-10);
-	}
-);
+/*********** Scrolling ***********/
+
+function scrollTo(storylineIndex){
+	$('.story-container').scrollTop($('.story-line[turn=\''+storylineIndex+'\']').get(0).offsetTop-10);
+}
+
+$('.axislabel').click( function(){
+	scrollTo($(this).index())
+});
+
+$('.story-sidebar-block').click( function(){
+	scrollTo($(this).index())
+});
 
 
-$('.controlbox, .axislabel, .valuebox').hover(
-	function(){	$(this).addClass('highlight')},
-	function(){	$(this).removeClass('highlight')}
-);
+/*********** Controls Hovering ***********/
 
-$('.controlbox').click(
-	function(){ $('.controlbox').addClass('faded');
-				$(this).removeClass('faded');
-				$('.graph-container').css('display','none');
-				$('.graph-container[graphname=\''+$(this).attr('control')+'\']').css('display','flex');}
-);
-$('.modalcontrolbox').click(
-	function(){
-		$(this).toggleClass('faded');
-		$('.modal.'+$(this).attr('control')).toggle();
-	}
-);
+$('.controlbox').click(function(){ 
+	$('.controlbox').addClass('faded');
+	$(this).removeClass('faded');
 
-$('.graph-control').click(
-	function(){	var i = $(this).index('.graph-control');
-	if (i==0){
+	$('.graph-container').css('display','none');
+	$('.graph-container[graphname=\''+$(this).attr('control')+'\']').css('display','flex');
+});
+
+$('.modalcontrolbox').click(function(){
+	$(this).toggleClass('faded');
+	$('.modal.'+$(this).attr('control')).css('opacity', 1-$('.modal.'+$(this).attr('control')).css('opacity'));
+});
+
+$('.graph-control').click(function(){		
+	if ($(this).index('.graph-control')==0){
 		$('.row').toggleClass('topshifted');
 		$('.axis').toggleClass('topshifted');
 		$('.row').removeClass('downshifted');
 		$('.axis').removeClass('downshifted');
-	}else{
+	}
+	else{
 		$('.row').toggleClass('downshifted');
 		$('.axis').toggleClass('downshifted');
 		$('.row').removeClass('topshifted');
 		$('.axis').removeClass('topshifted');
 	}
+
 	$('.graph-control').not(this).removeClass('highlight');
 	$(this).toggleClass('highlight');}
 );
 
 
-const psg = new PerfectScrollbar('.graph', {useBothWheelAxes:true,suppressScrollY:true});
+const psg = new PerfectScrollbar('.graph', {useBothWheelAxes:true, suppressScrollY:true});
 const psleg = new PerfectScrollbar('.legend');
 const psc = new PerfectScrollbar('.controls');
-const pslog = new PerfectScrollbar('.story-container',{suppressScrollX:true});
+const pslog = new PerfectScrollbar('.story-container', {suppressScrollX:true});
