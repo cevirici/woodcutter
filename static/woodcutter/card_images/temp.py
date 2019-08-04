bc175107c6693c4bfd90af40c91bdc04785ac007
ceviri <bleh3@hotmail.com>
2019-08-03 23:45:01 -0400

    Captain/Church image url

--- static/woodcutter/card_images/temp.py
+++ static/woodcutter/card_images/temp.py
@@ -6,9 +6,15 @@ fsplit = [x.strip().split(':') for x in f]
 urls = [url.split('/')[-1] for card, url in fsplit]
 
 f.close()
-for url in urls:
+for url in urls[-3:]:
+    print(url)
     try:
         im = Image.open(url)
+        size = im.size
+        size = [coord // 3 for coord in size]
+        im.thumbnail(size, Image.ANTIALIAS)
+        bits = url.split('.')
+        im.save(bits[0] + '-mid.' + bits[1])
         size = im.size
         size = [coord // 5 for coord in size]
         im.thumbnail(size, Image.ANTIALIAS)
