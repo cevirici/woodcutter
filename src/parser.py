e64c27b00f2cc2c1e922e1c0a2a74c9bd8e6f94a
ceviri <bleh3@hotmail.com>
2019-08-03 23:15:42 -0400

    Debug

--- src/parser.py
+++ src/parser.py
@@ -89,6 +89,7 @@ def translate_file(inString):
 def combined_parse(inStrings):
     logs = [translate_file(x) for x in inStrings]
     parsedLog = []
+    something = logs[0][0].arguments[0]
     gameNum = int(logs[0][0].arguments[0])
 
     actualLength = min([len(log) for log in logs])
