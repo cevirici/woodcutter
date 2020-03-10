5bb088d0715c38439a8a825d452ed7556763ca12
ceviri <bleh3@hotmail.com>
2020-03-10 19:10:39 -0400

    Print supply

--- src/GenericActions.py
+++ src/GenericActions.py
@@ -29,61 +29,61 @@ class startGame(Action):
             return None
 
 
-def startDecks(state, log):
-    # Presumes correctness.
-    state = deepcopy(state)
-    state.player = getOwner(log[state.logLine])
+# def startDecks(state, log):
+#     # Presumes correctness.
+#     state = deepcopy(state)
+#     state.player = getOwner(log[state.logLine])
 
-    items = getItems(log[state.logLine])
-    src = Cards[items.keys()[0]].initialZone
-    state.moveCards(items, src, PlayerZones.DISCARD)
-    state.logLine += 1
-    if getPred(log[state.logLine]) == 23:
-        state.candidates = [startDecks]
-    else:
-        state.candidates = [drawN(5)]
-        state.resolutionStack += [newTurn, drawN(5)]
-    return state
-
-
-def popResolutionStack(state, log):
-    event = state.resolutionStack.pop()
-    return event(state, log)
+#     items = getItems(log[state.logLine])
+#     src = Cards[items.keys()[0]].initialZone
+#     state.moveCards(items, src, PlayerZones.DISCARD)
+#     state.logLine += 1
+#     if getPred(log[state.logLine]) == 23:
+#         state.candidates = [startDecks]
+#     else:
+#         state.candidates = [drawN(5)]
+#         state.resolutionStack += [newTurn, drawN(5)]
+#     return state
 
 
-def drawN(n):
-    # n should be positive
-    def outFn(state, log):
-        state = deepcopy(state)
-        logLine = log[state.logLine]
-        state.player = getOwner(logLine)
-        deckCount = state.zoneCount(PlayerZones.DECK)
-        if deckCount < n:
-            if deckCount > 0:
-                if getPred(logLine != 8):
-                    raise LogMismatch
-                state.moveCards(getItems(logLine),
-                                PlayerZones.DECK, PlayerZones.HAND)
-                state.logLine += 1
-
-            if getPred(logLine != 28):
-                raise LogMismatch
-            items = state.zones[PlayerZones.DISCARD][state.player][0].cards
-            state.moveCards(items, PlayerZones.DISCARD, PlayerZones.DECK)
-            state.logLine += 1
-
-        if state.zoneCount(PlayerZones.DECK) > 0:
-            if getPred(logLine != 8):
-                raise LogMismatch
-            state.moveCards(getItems(logLine),
-                            PlayerZones.DECK, PlayerZones.HAND)
-            state.logLine += 1
-
-        return popResolutionStack(state, log)
-    return outFn
+# def popResolutionStack(state, log):
+#     event = state.resolutionStack.pop()
+#     return event(state, log)
 
 
-def newTurn(state, log):
-    state = deepcopy(state)
-    state.logLine += 900
-    return state
+# def drawN(n):
+#     # n should be positive
+#     def outFn(state, log):
+#         state = deepcopy(state)
+#         logLine = log[state.logLine]
+#         state.player = getOwner(logLine)
+#         deckCount = state.zoneCount(PlayerZones.DECK)
+#         if deckCount < n:
+#             if deckCount > 0:
+#                 if getPred(logLine != 8):
+#                     raise LogMismatch
+#                 state.moveCards(getItems(logLine),
+#                                 PlayerZones.DECK, PlayerZones.HAND)
+#                 state.logLine += 1
+
+#             if getPred(logLine != 28):
+#                 raise LogMismatch
+#             items = state.zones[PlayerZones.DISCARD][state.player][0].cards
+#             state.moveCards(items, PlayerZones.DISCARD, PlayerZones.DECK)
+#             state.logLine += 1
+
+#         if state.zoneCount(PlayerZones.DECK) > 0:
+#             if getPred(logLine != 8):
+#                 raise LogMismatch
+#             state.moveCards(getItems(logLine),
+#                             PlayerZones.DECK, PlayerZones.HAND)
+#             state.logLine += 1
+
+#         return popResolutionStack(state, log)
+#     return outFn
+
+
+# def newTurn(state, log):
+#     state = deepcopy(state)
+#     state.logLine += 900
+#     return state
